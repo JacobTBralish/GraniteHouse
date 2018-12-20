ad9eb5c81973f74da11e1c7cc2502a399da26b6a
Jacob <j.t.bralish@gmail.com>
2018-12-19 20:52:25 -0800

    Completed productTypes before special tags

--- GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -30,6 +30,7 @@ namespace GraniteHouse.Areas.Admin.Controllers
         }
 
         // GET create action method
+        // This takes you to the correct view which is the create.cshtml
         public IActionResult Create()
         {
             return View();
@@ -38,12 +39,12 @@ namespace GraniteHouse.Areas.Admin.Controllers
         /* ValidateAntiForgeryToken is a token that is sent with the HttpPost request to the server where the server
          checks if the token is valid or not. If valid it knows that the request has not been changed by any hackers in 
          any way. Comes with ASP.NET*/
-         // POST create action method
+        // POST create action method
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ProductTypes productTypes)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _db.Add(productTypes);
                 await _db.SaveChangesAsync();
@@ -54,6 +55,99 @@ namespace GraniteHouse.Areas.Admin.Controllers
             }
             // If there is an error we will return to the view and you pass it the product types so it can display the error message
             return View(productTypes);
+        }
+        // GET Edit action method. Must use Task<blah> when using the await keyword
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(productType);
+        }
+
+        // POST edit action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProductTypes productTypes)
+        {
+            if (id != productTypes.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                // You only can use the update method in the case that you are not updating tons of different rows and just a few
+                // because you do not want to update everything pointlessly
+                _db.Update(productTypes);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            // If there is an error we will return to the view and you pass it the product types so it can display the error message
+            return View(productTypes);
+        }
+
+        // GET Details action method. Must use Task<blah> when using the await keyword
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(productType);
+        }
+
+        // GET Delete action method. Must use Task<blah> when using the await keyword
+        // We place the nullable in the parameters because the id actually can be nullable in an off chance. If nullable is there
+        // we must provide a check in the method to see wether or not the id is in fact null.
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(productType);
+        }
+
+        // POST Delete action method
+        // If you define action name here you can change the name of the POST action method to whatever you want. The reason
+        // it throws an error is because the GET and POST will both have the same parameters. It threw an error initally because
+        // we took out the nullable declaration after the int (which wasn't entirely necessary).
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productTypes = await _db.ProductTypes.FindAsync(id);
+            _db.ProductTypes.Remove(productTypes);
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
