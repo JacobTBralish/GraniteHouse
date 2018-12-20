d89c421e9eac24c20d155ada32528273320dc3ad
Jacob <j.t.bralish@gmail.com>
2018-12-20 00:40:04 -0800

    Create controller comeplete and functional, view complete

---
+++ GraniteHouse/Areas/Admin/Controllers/SpecialTagsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SpecialTagsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SpecialTagsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.SpecialTags.ToList());
+        }
+
+        // GET create action method
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SpecialTags specialTags)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(specialTags);
+                await _db.SaveChangesAsync();
+                return Redirect(nameof(Index));
+            }
+            return View(specialTags);
+        }
+
+
+    }
+}
